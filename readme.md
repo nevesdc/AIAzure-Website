7c9d93b05cd451f4097f09c33eaa06bd24123c30
Alexandre Neves <alexandre.a.t.neves@gmail.com>
2021-03-09 17:26:25 -0300

    primeiro commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Cognitive Services Lab
+This is a hands on lab guide for Azure. In this lab you will deploy a serverless application which uses Azure Cognitive Services to analyze photos gathered from twitter. An Azure Logic App drives the process and carries out most of the tasks. 
+
+The Logic App flow is:
+- Calls the Twitter API and searches for tweets containing a certain hashtag
+- Calls the Azure cognitive service API for each photo and gets the result which is a description of the contents of the photo
+- Stores the result in Azure Cosmos DB
+
+The Azure cognitive service uses a pre-trained computer vision model to return results describing the image as a JSON object. Cosmos DB is a No-SQL database, which the Logic App uses to store the results as JSON documents, one for each photo result.
+
+The final part of the application is a simple web app, written in Node.js. This web app is hosted in Azure as an Web App Service, it connects to Cosmos DB and displays the photo analysis results as a simple web page.
+
+
+
---
+++ webapp/routes/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const documentClient = require("documentdb").DocumentClient;
+const client = new documentClient(process.env.DB_ENDPOINT, {
+   "masterKey": process.env.DB_KEY
+});
+
+// ====================================================================================================
+// GET home page (index.ejs)
+// ====================================================================================================
+router.get('/', function(req, res, next) {
+  // Check database & collection settings
+  if(!process.env.DB_NAME) {
+    process.env.DB_NAME = "dbdesafio";
+  }
+  if(!process.env.DB_COLLECTION) {
+    process.env.DB_COLLECTION = "twitter";
+  }
+
+  const collectionUrl = ("/dbs/"+process.env.DB_NAME+"/colls/"+process.env.DB_COLLECTION);
+  console.log(collectionUrl);
+  
+  let photoResults = [];
+
+  // Query docs for photos with URL in reverse time order
+  client.queryDocuments(collectionUrl, 'SELECT TOP 50 * FROM p WHERE p.url != null AND p.description != null ORDER BY p.time DESC', { enableCrossPartitionQuery: true })
+  .toArray((err, results) => {
+    if(err) {
+      sendError(res, err);
+      return;
+    } else {
+
+      // Loop through results
+      for(let photo of results) {
+        // Push details into output array
+        photoResults.push({
+          url: photo.url, 
+          caption: titleCase(photo.description.captions[0].text),
+          confidence: Math.round(parseFloat(photo.description.captions[0].confidence) * 100),
+          tags: photo.description.tags
+        });
+      }
+
+      // Send result to browser
+      res.render('index', { 
+        title: 'Azure Cognitive Services - Computer Vision Demo',
+        photoResults: photoResults
+      });
+    }
+  });
+});
+
+// ====================================================================================================
+// Convert string to Title Case
+// ====================================================================================================
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return word.replace(word[0], word[0].toUpperCase());
+  }).join(' ');
+}
+
+// ====================================================================================================
+// Simple error page wrapper
+// ====================================================================================================
+function sendError(res, error) {
+  console.dir(error);
+  
+  res.render('error', { 
+    message: error.message,
+    error: JSON.stringify(error, null, 2)
+  });
+}
+
+module.exports = router;
